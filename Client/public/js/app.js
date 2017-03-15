cb060f710432f569d3c1a293793e5f0cad2ada1c
slemaire <slemaire@E117-03.CDF36.local>
2017-03-15 14:16:25 +0100

    mise en marche du routeur

--- Client/public/js/app.js
+++ Client/public/js/app.js
@@ -1,5 +1,37 @@
 'use strict';
 
-var blog = angular.module('portfolio', []);
+var blog = angular.module('blog', ['ngRoute']);
+blog.config(['$routeProvider',
+    function ($routeProvider){
+        $routeProvider
+                .when("/blog",{
+                    templateUrl:"public/pages/blog.html",
+                    controller:'blogCtrl'
+                })
+                .when("/contact",{
+                    templateUrl:"public/pages/contact.html",
+                    controller:'blogCtrl'
+                })
+                .when("/cv",{
+                    templateUrl:"public/pages/cv.html",
+                    controller:'blogCtrl'
+                })
+                .when("/insta",{
+                    templateUrl:"public/pages/insta.html",
+                    //controller:'blogCtrl'
+                })
+                .when("/projetsPerso",{
+                    templateUrl:"public/pages/projetsPerso.html",
+                    //controller:'blogCtrl'
+                })
+                .when("/projetsPro",{
+                    templateUrl:"public/pages/projetsPro.html",
+                    //controller:'blogCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/blog'
+                });
+            }]);
+
 
 
--- Client/public/js/controllers/blogController.js
+++ Client/public/js/controllers/blogController.js
@@ -1,4 +1,6 @@
 blog.controller('blogCtrl', function ($scope, $http) {
-   
+  $scope.hello = "hello World !";
+  $scope.machin = "hello Machin !";
+  $scope.bidule = "coucou c'est moi bidule !";
 });
 
